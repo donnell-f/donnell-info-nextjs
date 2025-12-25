149ecf067b9f9ee7f1e525417e818794624f38c2
Donnell Fulwiler <dbfulwiler@gmail.com>
2025-12-25 02:52:36 -0600

    Almost done with about page... probably

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -55,33 +55,50 @@ export default function AboutPage() {
     return (
         <>
             <main className="pt-18">
-                <section id="hero" className="max-w-[1200px] mx-auto grid grid-rows-[2rem_minmax(10rem,1fr)_2rem] grid-cols-[minmax(10rem,1fr)_2rem_1fr_2rem]">
+                <section id="hero" className="
+                    max-w-[1200px] mx-auto grid
+                    lg:grid-rows-[2rem_minmax(10rem,1fr)_2rem] lg:grid-cols-[minmax(10rem,1fr)_2rem_1fr_2rem]
+                    max-lg:grid-rows-[minmax(10rem,1fr)_2rem_minmax(10rem,1fr)] max-lg:grid-cols-[2rem_minmax(10rem,1fr)_2rem]
+                ">
                     <div id="heroPic" className="
-                        row-[2/2] col-[1/span_2]
+                        lg:row-[2/2] lg:col-[1/span_2]
+                        max-lg:row-[1/span_2] max-lg:col-[2/2]
                         bg-[url('/sailing_pic.jpeg')] bg-cover rounded-2xl bg-center min-h-[30rem]
-                        hover:-translate-x-16 transition-all duration-300 ease-in-out
+                        hover:-translate-x-16 transition-all duration-300 ease-in-ou
                         hover:shadow-2xl
                     "></div>
                     <div id="heroDesc" ref={heroDesc} className="
-                        p-8 row-[1/span_3] col-[2/span_3] bg-linear-60 from-green-100 to-teal-300 rounded-2xl z-10
+                        max-lg:row-[2/span_2] max-lg:col-[1/span_3]
+                        lg:row-[1/span_3] lg:col-[2/span_3]
+                        p-8 bg-linear-60 from-green-100 to-teal-300 rounded-2xl z-10
                     ">
                         <h1 className="text-center text-4xl font-semibold my-8">About Me</h1>
                         <p className="text-center text-xl mx-8">Howdy! I am a Computer Science Major at Texas A&M University. I am originally from Austin, TX.</p>
                     </div>
                 </section>
                 <div id="spacer1" className="my-18"></div>
-                <section className="max-w-[1200px] mx-auto grid grid-rows-[2rem_minmax(10rem,1fr)_2rem] grid-cols-[2rem_1fr_2rem_minmax(10rem,1fr)]">
+                <section className="
+                    max-w-[1200px] mx-auto grid
+                    lg:grid-rows-[2rem_minmax(10rem,1fr)_2rem] lg:grid-cols-[2rem_1fr_2rem_minmax(10rem,1fr)]
+                    max-lg:grid-rows-[minmax(10rem,1fr)_2rem_minmax(10rem,1fr)] max-lg:grid-cols-[2rem_minmax(10rem,1fr)_2rem]
+                ">
                     <div id="techPic" className="
-                        row-[2/2] col-[3/span_2]
+                        lg:row-[2/2] lg:col-[3/span_2]
+                        max-lg:row-[1/span_2] max-lg:col-[2/2]
                         bg-[url('/tech.png')] bg-cover rounded-2xl bg-center min-h-[30rem]
                         hover:translate-x-16 transition-all duration-300 ease-in-out
                         hover:shadow-2xl
                     "></div>
                     <div id="heroDesc2" ref={techDesc} className="
-                        p-8 row-[1/span_3] col-[1/span_3] bg-linear-170 from-green-100 to-teal-300 rounded-2xl z-10
+                        max-lg:row-[2/span_2] max-lg:col-[1/span_3]
+                        lg:row-[1/span_3] lg:col-[1/span_3]
+                        p-8 bg-linear-170 from-green-100 to-teal-300 rounded-2xl z-10
                     ">
                         <h1 className="text-center text-4xl font-semibold my-8">Tech Stack</h1>
-                        <p className="text-center text-xl mx-8">I built this website in Next.js using Javascript, Tailwind, and NPM. I chose Next mainly because of its built-in SSR, which is helpful for SEO.</p>
+                        <p className="text-center text-xl mx-8">I built this
+                        website in Next.js using Javascript, Tailwind, and NPM.
+                        I chose Next mainly because of its built-in SSR, which
+                        is helpful for SEO.</p>
                         <div id="techSpacer1" className="my-20"></div>
                         <LogoLoop logos={techLogos} speed={15}/>
                     </div>
